1736766dc737deb43189e83decf14569255679fb
Msingh008 <msingh30497@gmail.com>
2025-07-31 15:26:19 +0530

    Update Runner.java

--- src/main/java/Utilities/Runner.java
+++ src/main/java/Utilities/Runner.java
@@ -3,7 +3,7 @@ package Utilities;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-
+//Cometing
 @CucumberOptions(
         features = "src/test/java/datatable.feature",
         glue = "stepdefenitions",
